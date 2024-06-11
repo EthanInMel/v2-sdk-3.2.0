d86478716d876ea150d76dc9be9f91da09dd539f
Yi Song (Ethan) <ethaninmel@gmail.com>
2024-06-11 19:55:37 +0800

    update initcodehash

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,6 +1,6 @@
 import JSBI from 'jsbi';
 export declare const FACTORY_ADDRESS = "0xB8a040f25890A80363c1D82f6eF23322aAb46dF1";
-export declare const INIT_CODE_HASH = "d791fed674bf5115485e28b3ecf3febe345200063a5d9056ce39654010787fef";
+export declare const INIT_CODE_HASH = "0xd791fed674bf5115485e28b3ecf3febe345200063a5d9056ce39654010787fef";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
