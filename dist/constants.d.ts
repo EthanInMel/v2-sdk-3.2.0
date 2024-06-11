61d24f9362a05031247067cc3cf7f987100b383f
Yi Song (Ethan) <ethaninmel@gmail.com>
2024-06-11 19:41:36 +0800

    build

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xB8a040f25890A80363c1D82f6eF23322aAb46dF1";
+export declare const INIT_CODE_HASH = "d791fed674bf5115485e28b3ecf3febe345200063a5d9056ce39654010787fef";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
